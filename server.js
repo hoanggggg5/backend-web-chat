4960b652a3d01a2ac4a72e82a81c6bcad3890c92
nghiemdanghoang <hoangkid02@gmail.com>
2021-06-08 15:09:16 +0700

    1.0

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const PORT = 7000;
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+app.get("/", (req, res) => {
+  res.status(200).json({ name: "server" });
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  console.log("Someone connected and socketId " + socket.id);
+
+  socket.on("disconnect", () => {
+    console.log(socket.id + "disconnected");
+    for (let user in users) {
+      if (users[user] == socket.id) {
+        delete users[user];
+      }
+    }
+
+    io.emit("all_users", users);
+  });
+
+  socket.on("new_user", (username) => {
+    console.log(username);
+    users[username] = socket.id;
+
+    io.emit("new_user", users);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`listening on: ${PORT}`);
+});
