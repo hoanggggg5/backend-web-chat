29d80acf24939d9f7a2a7c1cc540df22dfb9f87e
hoangdang <nghiemdanghoang>
2021-07-25 22:00:50 +0700

    'v2.0'

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const httpServer = require("http").createServer(app);
 const PORT = 7000;
 
 const io = require("socket.io")(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
 });
 
 // app.get("/", (req, res) => {
@@ -15,40 +15,41 @@ const io = require("socket.io")(httpServer, {
 // });
 
 app.get("/", (req, res) => {
-  res.status(200).json({ name: "server" });
+    res.status(200).json({ name: "server" });
 });
 
 const users = {};
 
 io.on("connection", (socket) => {
-  console.log("Someone connected and socketId " + socket.id);
+    console.log("Someone connected and socketId " + socket.id);
 
-  socket.on("disconnect", () => {
-    console.log(socket.id + "disconnected");
-    for (let user in users) {
-      if (users[user] == socket.id) {
-        delete users[user];
-      }
-    }
+    socket.on("disconnect", () => {
+        console.log(socket.id + "disconnected");
+        for (let user in users) {
+            if (users[user] == socket.id) {
+                delete users[user];
+            }
+        }
 
-    io.emit("all_users", users);
-  });
+        io.emit("all_users", users);
+    });
 
-  socket.on("new_user", (username) => {
-    console.log(username);
-    users[username] = socket.id;
+    socket.on("new_user", (username) => {
+        console.log(username);
+        users[username] = socket.id;
 
-    io.emit("new_user", users);
-  });
+        io.emit("new_user", users);
+    });
 
-  socket.on("send_message", (data) => {
-    console.log(data);
+    socket.on("send_message", (data) => {
+        console.log(data);
 
-    const socketId = users[data.receiver];
-    io.to(socketId).emit("new_message", data);
-  });
+        const socketId = users[data.receiver];
+        io.to(socketId).emit("new_message", data);
+    });
 });
 
 httpServer.listen(PORT, () => {
-  console.log(`listening on: ${PORT}`);
+    console.log(`listening on: ${PORT}`);
+    console.log("oki");
 });
